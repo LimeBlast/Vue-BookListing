1b8bd34b30f341904f81f73219d422b4080b0ec3
Daniel Hollands <daniel@limeblast.co.uk>
2018-10-09 13:28:19 +0100

    Creating a child component

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,10 +1,10 @@
 <template>
-<div>
-  <h1>{{title}}</h1>
-  <ul>
-    <li v-for="book in books">{{book.title}}: {{book.author}}</li>
-  </ul>
-</div>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">{{book.title}}: {{book.author}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -31,10 +31,5 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-
-li {
-  display: block;
-  margin: 0 10px;
 }
 </style>
